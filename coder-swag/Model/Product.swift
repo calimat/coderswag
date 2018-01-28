ad392c7d9f102120bfdcbe01e020852755d7ba23
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-01-28 11:06:25 -0500

    Added data for CollectionView

---
+++ coder-swag/Model/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Ricardo Herrera Petit on 1/28/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+struct Product {
+    private (set) public var title: String
+    private (set) public var price: String
+    private (set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String ) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
